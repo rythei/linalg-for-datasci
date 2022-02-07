90c8e340441977361341d62e58f80b52fe66dde6
rythei <rythei@Ryans-MacBook-Pro-3.local>
2022-02-07 07:54:32 -0800

    update norm-based classification

--- _build/jupyter_execute/content/basicLA_1_classification-with-norms.py
+++ _build/jupyter_execute/content/basicLA_1_classification-with-norms.py
@@ -17,17 +17,17 @@ plt.axis('equal')
 plt.legend()
 plt.show()
 
-Here we will attempt to develop a classifier which predicts a label for a datapoint $\boldsymbol{x}$ based on its $2$-norm. Specifically, we will consider classifiers of the form
+Here we will attempt to develop a classifier which predicts a label for a datapoint $\boldsymbol{x}$ based on its $p$-norm. Specifically, we will consider classifiers of the form
 
 $$
-C_t(\boldsymbol{x}) = \begin{cases}1 & \text{if }\|\boldsymbol{x}\|_2 \leq t\\ 0 & \text{if } \|\boldsymbol{x}\|_2 > t\end{cases}
+C_t(\boldsymbol{x}) = \begin{cases}1 & \text{if }\|\boldsymbol{x}\|_p \leq t\\ 0 & \text{if } \|\boldsymbol{x}\|_p > t\end{cases}
 $$
 
-where the threshold $t$ is to be determined. To see why this is a natural form of classifier to use, remember that the $2$-ball at radius $t$ is exactly a circle in $\mathbb{R}^2$, and since the data shown above are in the form of concentric circles, it makes sense that thresholding on the $2$-norm may give good classification performance.
+where the threshold $t$ is to be determined. To see why this is a natural form of classifier to use, remember that the $2$-ball at radius $t$ is exactly a circle in $\mathbb{R}^2$, and since the data shown above are in the form of concentric circles, it makes sense that thresholding on the $2$-norm may give good classification performance. However, we will also test the $1$-norm in this example to verify that the $2$-norm is indeed the most appropriate norm to use in this circumstance.
 
 It remains to determine the best value of threshold $t$ to use. To do this, we will try many values of $t$, and pick the one that makes the fewest mistakes on the dataset.
 
-First, we define a function `predict(X, y, t)` which takes in data `X,y` and a given threshold `t`, and returns the misclassification rate for the $2$-norm classifier with threshold $t$. The misclassification rate is just the fraction of datapoints that are misclassified by $C_t$:
+First, we define a function `predict(X, y, t, norm)` which takes in data `X,y`, a given threshold `t`, and which norm to use, and returns the misclassification rate for the given norm classifier with threshold $t$. The misclassification rate is just the fraction of datapoints that are misclassified by $C_t$:
 
 $$
 \text{Misclassification Rate} = \frac{1}{n}\sum_{i=1}^n \mathbb{1}(C_t(\boldsymbol{x}_i)\neq y_i).
@@ -37,9 +37,9 @@ The function is defined as follows.
 
 import numpy as np
 
-def predict(X, y, t):
+def predict(X, y, t, norm=2):
     # get the norm of each data point
-    norms = np.linalg.norm(X, axis=1)
+    norms = np.linalg.norm(X, ord=norm, axis=1)
 
     # get predictions by evaluating if the point
     # is above or below the threshold t
@@ -51,16 +51,17 @@ def predict(X, y, t):
 
 Next, we will evaluate the model at a variety of values of threshold $t$, and find which one has the lowest misclassification rate.
 
-t_range = np.arange(0.2,1.21,.01)
+t_range = np.arange(0.2,1.51,.01)
 
-errors = []
-for t in t_range:
-    err = predict(X, y, t)
-    errors.append(err)
-
-plt.plot(t_range, errors)
+for norm in [1,2]:
+    errors = []
+    for t in t_range:
+        err = predict(X, y, t, norm=norm)
+        errors.append(err)
+    plt.plot(t_range, errors, label=f'{norm}-norm')
+plt.legend()
 plt.xlabel('threshold, t')
 plt.ylabel('misclassification rate')
 plt.show()
 
-From the plot, we find that the best threshold is $t\approx 0.89$, and that at this value, the misclassification rate is $0.01$, or $1\%$.
+From the plot, we find that the best threshold is indeed attained using the $2$-norm with $t\approx 0.89$, and that at these values, the misclassification rate is $0.01$, or $1\%$.
